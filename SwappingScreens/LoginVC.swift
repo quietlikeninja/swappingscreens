5246be05ae9bbf4b485371de6147bd0a311eed1b
Justin Ward <justin@quietlike.ninja>
2017-02-26 13:00:08 +1000

    Initial commit

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Justin Ward on 26/2/17.
+//  Copyright © 2017 Justin Ward. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+
+}
+
---
+++ SwappingScreens/MusicListVC.swift
@@ -0,0 +1,37 @@
+//
+//  MusicListVCViewController.swift
+//  SwappingScreens
+//
+//  Created by Justin Ward on 26/2/17.
+//  Copyright © 2017 Justin Ward. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Dookie"
+        performSegue(withIdentifier: "SongVC", sender: songTitle)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            
+            if let song = sender as? String {
+                destination.selectedSong = song
+        }
+    }
+    }
+
+}
